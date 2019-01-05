2775becb91241f8becc12990942198ba1030479f
theinfamousAK <akhilkalepu@protonmail.com>
2019-01-05 16:09:17 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependecies
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
+const bodyParser = require("body-parser");
+
+// Set up a default port
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Configure mongoose
+mongoose.Promise = bluebird;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+});
+
+
+// Define API routes here
+var articlesController = require("./server/controllers/article-controller");
+var router = new express.Router();
+// Define any API routes first
+// Get saved articles
+router.get("/api/saved", articlesController.find);
+// Save articles
+router.post("/api/saved", articlesController.insert);
+// delete saved articles
+router.delete("/api/saved/:id", articlesController.delete);
+// Send every other request to the React app
+router.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.use(router);
+
+// Connect mongoose to our database
+const db = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(db, function(error) {
+  // Log any errors connecting with mongoose
+  if (error) {
+    console.error(error);
+  }
+  // Or log a success message
+  else {
+    console.log("mongoose connection is successful");
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
